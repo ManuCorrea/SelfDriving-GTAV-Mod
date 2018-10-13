9857c54b07cb4b9a7b3a9ecab4f5c23cb2d85438
manu correa <manu2025@live.com>
2018-10-13 14:39:46 +0200

    Agregar archivos de proyecto.

---
+++ SelfDrivingMod/Class1_Compare.cs
@@ -0,0 +1,319 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+using GTA.Math;
+using System.IO;
+using GTA.Native;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class ScriptTutorial : Script
+{
+    Camera cam;
+    Vector3 rot;
+    public Vector3 coordinates;
+    public GTA.Math.Vector3 offset;
+    public GTA.Math.Vector3 mapDestination;
+    OutputArgument direction = new OutputArgument(), vehicleOut = new OutputArgument(), distance = new OutputArgument();
+    Ped playerPed;
+    int variable = 60;
+    Rectangle bounds = new Rectangle(0, 0, 800, 600);
+    Bitmap bitmap;
+    Graphics g;
+
+    int width;
+    int height;
+    Size size;
+
+    string _path;
+    string text = "pos no";
+
+    Vector3 coords;
+    Boolean onRoad = false;
+    float velo;
+    string speed;
+    float heading;
+    string json;
+
+    byte magicNumber = 0;
+
+    string path = @"c:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining\data_file.json";
+
+    Point p1 = new Point();
+    long compare_time;
+    long save_time;
+
+    Bitmap prev_front;
+
+
+
+    public ScriptTutorial()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+
+        Interval = 1;
+
+        World.DestroyAllCameras();
+        cam = World.CreateCamera(new Vector3(1956.543f, 3699.894f, 35), Vector3.Zero, 60);
+        cam.IsActive = true;
+
+        width = bounds.Width;
+        height = bounds.Height;
+        size = bounds.Size;
+        bitmap = new Bitmap(width, height);
+        g = Graphics.FromImage(bitmap);
+        p1.X = 0;
+        p1.Y = 30;
+
+               
+        //GTA.Native.Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, cam.Handle, true, true);
+
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+        UI.ShowSubtitle("loaded");
+        if (magicNumber == 5)
+        {
+            if (variable == 60)
+            {
+                variable = 0;
+
+            }
+            else
+            {
+                variable += 60;
+
+            }
+            magicNumber = 0;
+        }
+
+        playerPed = Game.Player.Character;
+        Vehicle vehicle = playerPed.CurrentVehicle;
+
+
+        if (playerPed.IsInVehicle())
+        {
+            //GPS & Coords
+            mapDestination = World.GetWaypointPosition(); //https://github.com/Ric4o/GTAV-EnhancedNativeTrainer/blob/master/EnhancedNativeTrainer/src/features/teleportation.cpp
+            mapDestination.Z = World.GetGroundHeight(mapDestination);
+            Function.Call(Hash.GENERATE_DIRECTIONS_TO_COORD, mapDestination.X, mapDestination.Y, mapDestination.Z, 1, direction, vehicleOut, distance);
+
+
+            velo = vehicle.Speed;
+            speed = velo.ToString();
+            heading = vehicle.Heading;
+            rot = vehicle.Rotation;
+            coordinates = vehicle.Position;
+
+            onRoad = Function.Call<bool>(Hash.IS_POINT_ON_ROAD, coordinates.X, coordinates.Y, coordinates.Z, vehicle);
+
+
+            World.DestroyAllCameras();
+            cam = World.CreateCamera(new Vector3(1956.543f, 3699.894f, 35), Vector3.Zero, 60);
+            cam.IsActive = true;
+
+            rot = vehicle.Rotation;
+
+            cam.AttachTo(vehicle, new Vector3(0f, 2f, 1f));
+
+            Function.Call(Hash.SET_CAM_ROT, cam, rot.X, rot.Y, rot.Z + variable, 2);
+            GTA.Native.Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, cam.Handle, true, true);
+
+            try
+            {
+                if (variable == 0 && (magicNumber == 3 || magicNumber == 4 || magicNumber == 2))
+                {
+
+                    g.CopyFromScreen(p1, Point.Empty, size);
+                    //C:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining
+
+                    Stopwatch sw = new Stopwatch();
+
+                    sw.Start();
+                    bool iguales = CompareBitmapsFast(bitmap, prev_front);
+                    sw.Stop();
+                    compare_time = sw.ElapsedMilliseconds;
+                    if (iguales == false)
+                    {
+                        sw.Restart();
+                        bitmap.Save(@"c:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining\test.bmp", ImageFormat.Bmp);
+                        prev_front = (Bitmap)bitmap.Clone();
+
+                        sw.Stop();
+                        save_time = sw.ElapsedMilliseconds;
+                    }
+                    else
+                    {
+                        save_time = 0;
+                    }
+
+                    
+
+                    //Stopwatch para tiempo salvar y otro tiempo comparar
+
+                }
+                else if (variable == 60 && (magicNumber == 3 || magicNumber == 4 || magicNumber == 2))
+                {
+                    g.CopyFromScreen(p1, Point.Empty, size);
+                    bitmap.Save(@"c:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining\tost.bmp", ImageFormat.Bmp);
+                }
+
+            }
+            catch(Exception exeption)
+            {
+                UI.Notify(exeption.ToString());
+                UI.Notify("error");
+            }
+
+            try
+            {
+                json = File.ReadAllText(path);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                jsonObj["car"]["speed"] = velo;
+                jsonObj["car"]["incar"] = true;
+                jsonObj["car"]["coordsX"] = coordinates.X.ToString();
+                jsonObj["car"]["coordsY"] = coordinates.Y.ToString();
+                jsonObj["car"]["coordsZ"] = coordinates.Z.ToString();
+                jsonObj["car"]["heading"] = rot.Z.ToString();
+                jsonObj["car"]["direction"] = direction.GetResult<int>().ToString();
+                jsonObj["car"]["on_road"] = onRoad;
+                jsonObj["car"]["path"] = text;
+                jsonObj["car"]["time_compare"] = compare_time.ToString();
+                jsonObj["car"]["time_save"] = save_time.ToString();
+
+
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(path, output);
+            }
+            finally
+            {
+
+            }
+            
+
+            magicNumber += 1;
+
+        }
+        else
+        {
+            World.DestroyAllCameras();
+            Function.Call(Hash.DETACH_CAM, cam);
+            Function.Call(Hash.RENDER_SCRIPT_CAMS, 0, 0, cam.Handle, 0, 0);
+            World.RenderingCamera = cam;
+            cam.AttachTo(playerPed, playerPed.Position);
+            rot = playerPed.Rotation;
+            Function.Call(Hash.SET_CAM_ROT, cam, rot.X, rot.Y, rot.Z, 2);
+            Function.Call(Hash.GET_RENDERING_CAM, cam);
+
+            
+            //UI.Notify("no en coche man");
+
+            json = File.ReadAllText(path);
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            jsonObj["car"]["incar"] = false;
+            //Console.ReadKey();
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(path, output);
+
+        }
+    }
+
+    void OnKeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+
+    public void mountCameraOnVehicle()
+    {
+        if (Game.Player.Character.IsInVehicle())
+        {
+            GTA.Native.Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, cam.Handle, true, true);
+        }
+        else
+        {
+            UI.Notify("Please enter a vehicle.");
+        }
+    }
+
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.I)
+        {
+            mountCameraOnVehicle();
+        }
+    }
+
+    public static bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
+    {
+        int counter = 0;
+        if (bmp1 == null || bmp2 == null)
+            return false;
+
+        if (object.Equals(bmp1, bmp2))
+            return true;
+
+        if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+            return false;
+
+        int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+        bool result = true;
+        byte[] b1bytes = new byte[bytes];
+        byte[] b2bytes = new byte[bytes];
+
+        BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width - 1, bmp1.Height - 1), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+        BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width - 1, bmp2.Height - 1), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+        Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+        Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+        for (int n = 0; n <= bytes - 1; n++)
+        {
+            if (b1bytes[n] != b2bytes[n])
+            {
+                result = false;
+                break;
+            }
+        }
+
+        bmp1.UnlockBits(bitmapData1);
+        bmp2.UnlockBits(bitmapData2);
+
+        return result;
+    }
+
+    public static bool CompareBitmapsLazy(Bitmap bmp1, Bitmap bmp2)
+    {
+        int counter = 0;
+        if (bmp1 == null || bmp2 == null)
+            return false;
+
+        if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+            return false;
+
+        //Compare bitmaps using GetPixel method
+        for (int column = 0; column < bmp1.Width; column++)
+        {
+            for (int row = 0; row < bmp1.Height; row++)
+            {
+                if (!bmp1.GetPixel(column, row).Equals(bmp2.GetPixel(column, row)))
+                    counter += 1;
+                    if (counter > 1000)
+                    {
+                        return false;
+                    }
+                    
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ SelfDrivingMod/Mod.cs
@@ -0,0 +1,258 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+using GTA.Math;
+using System.IO;
+using GTA.Native;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class ScriptTutorial : Script
+{
+    //Player info
+    Ped playerPed;
+    Vehicle vehicle;
+
+    //Camera
+    Camera cam;
+    Vector3 rot;
+
+    //Map data
+    public Vector3 coordinates;
+    public GTA.Math.Vector3 offset;
+    public GTA.Math.Vector3 mapDestination;
+    OutputArgument direction = new OutputArgument(), vehicleOut = new OutputArgument(), distance = new OutputArgument();
+    
+    int camAngle = 0;
+    Rectangle bounds = new Rectangle(0, 0, 800, 480);
+    Bitmap bitmap;
+    Graphics g;
+
+    //Front Image variables
+    int width;
+    int height;
+    Size size;
+    Size size2;
+    Rectangle bounds2 = new Rectangle(0, 0, 600, 300);
+
+    //Left Image variables
+    Bitmap bitmap2;
+    Graphics g2;
+    int width2;
+    int height2;
+
+    Boolean onRoad = false;
+    float speed;
+    float heading;
+    string json;
+
+    //Interval
+    byte imageInterval = 0;
+    readonly byte endInterval = 5;
+
+    //Top left point in the rectangle that take the screenshot
+    Point p1 = new Point(0, 150);
+    Point p2 = new Point(0, 250);
+
+    bool simulate_cameras = false;
+    string path;
+
+
+    public ScriptTutorial()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+
+        World.DestroyAllCameras();
+        cam = World.CreateCamera(new Vector3(1956.543f, 3699.894f, 35), Vector3.Zero, 60);
+        cam.IsActive = true;
+
+        width = bounds.Width;
+        height = bounds.Height;
+
+        width2 = bounds2.Width;
+        height2 = bounds2.Height;
+        
+        size = bounds.Size;
+        size2 = bounds2.Size;
+        bitmap = new Bitmap(width, height);
+        bitmap2 = new Bitmap(width2, height2);
+        g = Graphics.FromImage(bitmap);
+        g2 = Graphics.FromImage(bitmap2);
+
+        string ruta = Directory.GetCurrentDirectory();
+        string combined = Path.Combine(ruta, "scripts");
+
+        string texto = Path.Combine(combined, Path.GetFileName("path.txt"));
+
+        path = System.IO.File.ReadAllText(@texto);
+        string image1 = Path.Combine(texto, Path.GetFileName("front.bmp"));
+        string image2 = Path.Combine(texto, Path.GetFileName("left.bmp"));
+
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+        if (simulate_cameras == true)
+        {
+            if (imageInterval == endInterval)
+            {
+                if (camAngle == 60)
+                {
+                    camAngle = 0;
+                }
+                else
+                {
+                    camAngle += 60;
+                }
+                imageInterval = 0;
+            }
+        }
+        else
+        {
+            imageInterval = 2;
+        }
+
+
+        playerPed = Game.Player.Character;
+        
+
+        if (playerPed.IsInVehicle())
+        {
+            GetGameData();
+            ManageGameCamera();
+
+            try
+            {
+                //I know those if statements looks inefficient, if I make them "properly" it goes fast enough to capture the camAngle they aren't suppose
+                //to capture(at least in my PC). Just try changging the accepted imageInterval to see what is the best for you.
+                if (camAngle == 0 && (imageInterval == 2 || imageInterval == 4 || imageInterval == 5))
+                {
+                    g.CopyFromScreen(p1, Point.Empty, size);
+                    bitmap.Save(@"c:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining\test.bmp", ImageFormat.Bmp);
+                }
+                else if (camAngle == 60 && (imageInterval == 2 || imageInterval == 4))
+                {
+                    g2.CopyFromScreen(p2, Point.Empty, size2);
+                    bitmap2.Save(@"c:\Users\yo\Desktop\SelfDriving\joystickrecolDataTraining\tost.bmp", ImageFormat.Bmp);
+                }
+                imageInterval += 1;
+
+            }
+            catch
+            {
+                //It doesn't matters if previously safe fails.
+            }
+
+            try
+            {
+                json = File.ReadAllText(path);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                jsonObj["car"]["speed"] = speed;
+                jsonObj["car"]["incar"] = true;
+                jsonObj["car"]["coordsX"] = coordinates.X;
+                jsonObj["car"]["coordsY"] = coordinates.Y;
+                jsonObj["car"]["coordsZ"] = coordinates.Z;
+                jsonObj["car"]["heading"] = rot.Z;
+                jsonObj["car"]["direction"] = direction.GetResult<int>();
+                jsonObj["car"]["on_road"] = onRoad;
+                jsonObj["car"]["2cam"] = simulate_cameras;
+                jsonObj["car"]["destinationX"] = mapDestination.X;
+                jsonObj["car"]["destinationY"] = mapDestination.Y;
+                jsonObj["car"]["destinationZ"] = mapDestination.Z;
+
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(path, output);
+            }
+            catch
+            {
+            }          
+
+        }
+        else
+        {
+            OnFootCamera();
+                                              
+            json = File.ReadAllText(path);
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            jsonObj["car"]["incar"] = false;  
+
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(path, output);
+            }
+    }
+
+    public void MountCameraOnVehicle()
+    {
+        if (Game.Player.Character.IsInVehicle())
+        {
+            GTA.Native.Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, cam.Handle, true, true);
+        }
+        else
+        {
+            UI.Notify("Please enter a vehicle.");
+        }
+    }
+
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.I)
+        {
+            MountCameraOnVehicle();
+        }
+        if (e.KeyCode == Keys.X)
+        {
+            if (simulate_cameras == true){
+                simulate_cameras = false;
+                camAngle = 0;
+            }
+            else
+            {
+                simulate_cameras = true;
+            }
+        }
+    }
+
+    void OnFootCamera()
+    {
+        World.DestroyAllCameras();
+        Function.Call(Hash.DETACH_CAM, cam);
+        Function.Call(Hash.RENDER_SCRIPT_CAMS, 0, 0, cam.Handle, 0, 0);
+        World.RenderingCamera = cam;
+        cam.AttachTo(playerPed, playerPed.Position);
+        rot = playerPed.Rotation;
+        Function.Call(Hash.SET_CAM_ROT, cam, rot.X, rot.Y, rot.Z, 2);
+        Function.Call(Hash.GET_RENDERING_CAM, cam);
+    }
+
+    void GetGameData()
+    {
+        vehicle = playerPed.CurrentVehicle;
+        //GPS & Coords
+        mapDestination = World.GetWaypointPosition(); //https://github.com/Ric4o/GTAV-EnhancedNativeTrainer/blob/master/EnhancedNativeTrainer/src/features/teleportation.cpp
+        mapDestination.Z = World.GetGroundHeight(mapDestination);
+        Function.Call(Hash.GENERATE_DIRECTIONS_TO_COORD, mapDestination.X, mapDestination.Y, mapDestination.Z, 1, direction, vehicleOut, distance);
+
+        speed = vehicle.Speed;
+        heading = vehicle.Heading;
+        rot = vehicle.Rotation;
+        coordinates = vehicle.Position;
+
+        onRoad = Function.Call<bool>(Hash.IS_POINT_ON_ROAD, coordinates.X, coordinates.Y, coordinates.Z, vehicle);
+
+    }
+
+    void ManageGameCamera()
+    {
+        World.DestroyAllCameras();
+        cam = World.CreateCamera(new Vector3(1956.543f, 3699.894f, 35), Vector3.Zero, 60);
+        cam.IsActive = true;
+
+        cam.AttachTo(vehicle, new Vector3(0f, 2f, 1.2f));
+
+        Function.Call(Hash.SET_CAM_ROT, cam, rot.X, rot.Y, rot.Z + camAngle, 2);
+        GTA.Native.Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, cam.Handle, true, true);
+    }
+}
